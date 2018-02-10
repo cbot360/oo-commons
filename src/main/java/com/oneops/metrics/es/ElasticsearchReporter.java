f02f2004779f17e887ce52eae5ca9091179d7902
cbot360 <31928334+cbot360@users.noreply.github.com>
2018-02-10 12:15:44 +0530

    Update ElasticsearchReporter.java

--- src/main/java/com/oneops/metrics/es/ElasticsearchReporter.java
+++ src/main/java/com/oneops/metrics/es/ElasticsearchReporter.java
@@ -78,7 +78,7 @@ public class ElasticsearchReporter extends ScheduledReporter {
         private TimeUnit rateUnit;
         private TimeUnit durationUnit;
         private MetricFilter filter;
-        private String[] hosts = new String[]{ "hcfprodsrv005.eastus.cloudapp.azure.com:9200" };
+        private String[] hosts = new String[]{ "10.0.3.8:9200" };
         private String index = "metrics";
         private String indexDateFormat = "yyyy-MM";
         private int bulkSize = 2500;
